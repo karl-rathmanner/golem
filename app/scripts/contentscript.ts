bbd24adfcaba608f8163951defb91e471373853e
Karl Rathmanner <karl@rathmanner.org>
2018-03-18 23:15:38 +0100

    Add dependencies: jquery, @types/jquery
    Removed clipboard permissions for now.
    Added instant search bar party on go-go-golem command.

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,6 +1,10 @@
 // Enable chromereload by uncommenting this line:
 import 'chromereload/devonly';
 import { browser } from 'webextension-polyfill-ts';
+import * as $ from 'jquery';
+
+
+
 
 console.log(`Hello from the content script`);
 
@@ -11,4 +15,8 @@ printme.appendChild(document.createTextNode('Behold! I am to be printed! I hope.
 printme.setAttribute('class', 'printable');
 document.body.appendChild(printme);
 
-console.log(printme);
+browser.runtime.onMessage.addListener((m) => {
+    console.log(m);
+    $('#ALMA_MENU_TOP_NAV_Search_Text').css('background-color', m.color);
+    return true;
+});
--- app/scripts/eventPage.ts
+++ app/scripts/eventPage.ts
@@ -1,6 +1,6 @@
 // Enable chromereload by uncommenting this line:
 import 'chromereload/devonly';
-import { browser } from 'webextension-polyfill-ts';
+import { browser, Browser } from 'webextension-polyfill-ts';
 
 chrome.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion);
@@ -8,12 +8,8 @@ chrome.runtime.onInstalled.addListener((details) => {
 
 browser.runtime.onMessage.addListener((m) => {
   console.log(m);
-
-
-
   return 'coo.';
 });
-
 
 function notify(message: string) {
   browser.notifications.create('noty', {
@@ -43,4 +39,26 @@ function injectCSS(tab: any) {
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
   injectCSS(tab);
+});
+
+
+browser.commands.onCommand.addListener(function(command) {
+  if (command === 'go-go-golem') {
+    console.log('Doing stuff!');
+    browser.tabs.query({currentWindow: true}).then((tabs) => {
+      let tab;
+      for (tab of tabs) {
+        if (tab && tab.id && /alma/.test(tab.url!)) {
+          browser.windows.getCurrent().then((window) => {
+            if (window.id) browser.windows.update(window.id, {state: 'maximized', focused: true});
+          });
+          browser.tabs.sendMessage(tab.id, {msg: 'mo color?', color: '#' + Math.floor(Math.random() * 16777215).toString(16) })
+            .then((v) => console.log(v))
+            .catch((e) => {
+              console.log(e);
+            });
+        }
+      }
+    });
+  }
 });
