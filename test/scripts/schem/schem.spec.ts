71f9017f65ed08470116989ed873cd1aad9259b5
Karl Rathmanner <karl@rathmanner.org>
2018-04-07 01:46:43 +0200

    Rename describeRep to expectRep, change descriptions

--- test/scripts/schem/schem.spec.ts
+++ test/scripts/schem/schem.spec.ts
@@ -5,19 +5,34 @@ import { SchemNil, SchemType } from '../../../app/scripts/schem/types';
 import { pr_str } from '../../../app/scripts/schem/printer';
 
 /**
- * Describe a test case using repl lines (input) and testing the output (expected). If expected is an array, it has to
- * have the same length as input (counting 1 for a simple string). Optionally a description can be provided to make the
- * test case more informative.
- * @param {string | string[]} input
- * @param {string | string[]} expected
- * @param {string} description
- */
-let describeRep = (input: string | string[], expected: string | string[], description?: string) => {
+* Describe a test case using a Schem expression and its expected output. If expected is an array, it has to
+* have the same length as input (counting 1 for a simple string). Optionally a description can be provided to make the
+* test case more informative.
+* @param {string | string[]} input
+* @param {string | string[]} expected
+* @param {string} description
+*/
+let expectRep = (input: string | string[], expected: string | string[], description: string = '') => {
   let inputs = (typeof input === 'string') ? [input] : input;
   if (typeof expected !== 'string' && inputs.length !== expected.length) {
     throw Error(`length of the inputs (${inputs.length}) should math the expected one (${expected.length})`);
   }
-  it(description ? `${description}` : `"${inputs.join('; ')}" should evaluate to "${(typeof expected !== 'string') ? expected.join('; ') : expected}"`, function () {
+
+  if (description === '') {
+    if (expected instanceof Array) {
+      let multilineDescription: string[] = [];
+      expected.forEach((e, index: number) => {
+        multilineDescription.push(`${input[index]} => ${expected[index]}`);
+      });
+      description = multilineDescription.join('; ');
+    } else if (input instanceof Array) {
+      description = `${input.join('; ')}; should finally evaluate to => ${expected}`;
+    } else {
+      description = `${input} => ${expected}`;
+    }
+  }
+
+  it(description!, function () {
     let output;
     inputs.forEach((it, index) => {
       output = rep(it);
@@ -33,7 +48,7 @@ let describeRep = (input: string | string[], expected: string | string[], descri
 
 describe('blackbox tests', function() {
   // prn override, exposes values in prn_buffer instead of printing it to the browser console
-  before(() => {
+  before(function () {
     let prn_buffer = '';
     const envOverwrites: EnvSetupMap = {
       'prn': (...args: SchemType[]) => {
@@ -50,22 +65,29 @@ describe('blackbox tests', function() {
     expect(rep('true')).to.be.equal('true');
   });
 
-  describeRep('(+ 1 2)', '3');
+  expectRep('(+ 1 2)', '3');
 
-  describeRep('(list 1 2 3 4 5)', '(1 2 3 4 5)');
+  expectRep('(list 1 2 3 4 5)', '(1 2 3 4 5)');
 
-  describeRep('(vector 1 2 3 4 5)', '[1 2 3 4 5]');
+  expectRep('(vector 1 2 3 4 5)', '[1 2 3 4 5]');
 
-  describeRep(['(def! x 2)', '(+ x 1)'] , ['2', '3'], 'define a variable and use it in a following function');
+  expectRep(['(def! x 2)', '(+ x 1)'] , ['2', '3'], 'define a variable and use it in a following function');
 
-  describeRep([`
-      (def! sum (fn* (n acc)
-        (if (= n 0)
-          acc
-          (sum (- n 1) (+ n acc)
-      ))))
-    `, '(sum 4242 0)'] , '8999403', 'recursive function calls in tail position should not cause stack overflow');
+  expectRep([`
+    (def! sum (fn* (n acc)
+      (if (= n 0)
+        acc
+        (sum (- n 1) (+ n acc)
+      )
+    )))
+  `, '(sum 4242 0)'] , '8999403', 'recursive function calls in tail position should not cause stack overflow');
 
-  // add more tests here :)
+  expectRep(['(read-string "42")', '(read-string "\\"42\\"")'], ['42', '"42"']);
+
+  expectRep('(read-string "((fn* [x] (* x x)) 4)")', '((fn* [x] (* x x)) 4)');
+
+  expectRep(['(def! a 42)', '[a]'], '[42]');
+
+// add more tests here :)
 
 });
