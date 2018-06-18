fa0d7628bd03abd66a7ae0c9109b2f718d3fa432
Karl Rathmanner <karl@rathmanner.org>
2018-06-18 09:36:41 +0200

    feat(repl): add autocompletion

--- app/scripts/schemTest.ts
+++ app/scripts/schemTest.ts
@@ -1,17 +1,24 @@
 import * as $ from 'jquery';
 import { Key } from './utils/Key.enum';
 import { Schem } from './schem/schem';
-import { SchemBoolean, SchemType, SchemNil, SchemString } from './schem/types';
+import { SchemBoolean, SchemType, SchemNil, SchemString, SchemList, SchemSymbol, SchemKeyword } from './schem/types';
 import { EnvSetupMap } from './schem/env';
 import { pr_str } from './schem/printer';
 import { browser } from 'webextension-polyfill-ts';
 import { CommandHistory } from './utils/commandHistory';
 import { Settings } from './options';
+import { tokenize } from './schem/reader';
 
 $.when($.ready).then(() => {
-  const inputElement = $('input[name=input]');
+  const inputElement = $('#input');
   const commandHistory = new CommandHistory();
   const interpreter = new Schem();
+
+  let tokenAtCursorPosition: string;
+  let cursorPositionInToken: number;
+  let autocompleteSuggestions: string[] = [];
+  let selectedSuggestion: number = -1;
+  let lastManuallyTypedToken: string;
 
   Settings.loadSettings().then(s => {
     interpreter.arep(s.configScript, envOverwrites);
@@ -54,30 +61,121 @@ $.when($.ready).then(() => {
 
   inputElement.focus();
   inputElement.keydown((e) => {
-    switch (e.keyCode) {
+    if (e.ctrlKey) switch (e.keyCode) {
       case Key.Enter:
         commandHistory.resetHistoryPosition();
-        const input = $('input[name=input]').val() as string;
+        const input = inputElement.val() as string;
         commandHistory.addCommandToHistory(input);
 
-        // const repOutput = rep(input, envOverwrites);
         interpreter.arep(input, envOverwrites).then((result) => {
 
           $('#output').text($('#output').text() + result + currentSeparator);
           $('#output').animate({scrollTop: $('#output').prop('scrollHeight')}, 700);
         });
 
-        // $('#output').text($('#output').text() + repOutput + '\n');
-        $('input[name=input]').val('');
+        inputElement.val('');
         break;
 
       case Key.UpArrow:
-      commandHistory.previousCommand().then(v => $('input[name=input]').val(v));
+      commandHistory.previousCommand().then(v => inputElement.val(v));
         break;
       case Key.DownArrow:
-        $('input[name=input]').val(commandHistory.nextCommand());
+        inputElement.val(commandHistory.nextCommand());
+        break;
+    } else {
+      switch (e.keyCode) {
+        case Key.Tab:
+          if (autocompleteSuggestions.length > 0) {
+            selectedSuggestion += (e.shiftKey) ? -1 : 1;
+
+            if (selectedSuggestion >= autocompleteSuggestions.length) {
+              selectedSuggestion = -1;
+            }
+
+            let replaceWith: string = '';
+            if (selectedSuggestion < 0) {
+              replaceWith = lastManuallyTypedToken;
+            } else {
+              replaceWith = autocompleteSuggestions[selectedSuggestion];
+            }
+
+            insertAndDeleteRelativeToCursor(e.currentTarget, replaceWith, cursorPositionInToken, (tokenAtCursorPosition.length - cursorPositionInToken), replaceWith.length - cursorPositionInToken);
+          }
+          e.preventDefault();
+          break;
+        default:
+      }
+    }
+  });
+
+  inputElement.keypress((e) => {
+    if (!(e.keyCode === Key.Tab)) {
+      selectedSuggestion = -1;
+    }
+    switch (e.charCode) {
+      case 40: // (
+        insertAndDeleteRelativeToCursor(e.currentTarget, ')', 0);
+        break;
+      case 91: // [
+        insertAndDeleteRelativeToCursor(e.currentTarget, ']', 0);
+        break;
+      case 123: // {
+        insertAndDeleteRelativeToCursor(e.currentTarget, '}', 0);
+        break;
+      case 34: // "
+        insertAndDeleteRelativeToCursor(e.currentTarget, '"', 0);
         break;
     }
+  });
+
+  function insertAndDeleteRelativeToCursor(target: HTMLElement, insert: string, deleteBefore: number = 0, deleteAfter: number = 0, moveCursor: number = 0) {
+    const cursorPosition = ($(target).prop('selectionStart') as number);
+    const currentVal = (typeof $(target).val() === 'string') ? $(target).val() as string : '';
+    const newVal = currentVal.slice(0, cursorPosition - deleteBefore) + insert + currentVal.slice(cursorPosition + deleteAfter);
+    $(target).val(newVal);
+    (target as HTMLInputElement).setSelectionRange(cursorPosition + moveCursor, cursorPosition + moveCursor);
+  }
+
+  inputElement.on('change keyup paste', (e) => {
+
+    let currentInput = (typeof $(e.currentTarget).val() === 'string') ? $(e.currentTarget).val() as string : '';
+    const cursorPosition = $(e.currentTarget).prop('selectionStart') as number;
+    currentInput = currentInput.slice(0, cursorPosition) + '☺' + currentInput.slice(cursorPosition);
+    const tokens = tokenize(currentInput);
+
+    tokenAtCursorPosition = tokens.reduce((matched, token) => {
+      if (token.includes('☺')) {
+        cursorPositionInToken = token.indexOf('☺');
+        return token.replace('☺', '');
+      } else {
+        return matched;
+      }
+    }, '');
+
+    if (selectedSuggestion === -1) {
+      lastManuallyTypedToken = tokenAtCursorPosition;
+    }
+
+    interpreter.readEval(`(sortAndFilterByStringSimilarity "${lastManuallyTypedToken}" (listSymbols))`).then((suggestions) => {
+      if (suggestions instanceof SchemList) {
+        $('#autocompleteSuggestions').empty();
+        autocompleteSuggestions = [];
+        suggestions.map((s, i) => {
+          if (s instanceof SchemSymbol || s instanceof SchemString || s instanceof SchemKeyword) {
+            autocompleteSuggestions.push(s.stringValueOf());
+
+            $('#autocompleteSuggestions').append(`<span class="${(i === selectedSuggestion) ? 'highlightedSuggestion' : 'acSuggestion'}">${s.stringValueOf()}</span>`);
+            if (i < suggestions.length - 1) {
+              $('#autocompleteSuggestions').append('<span class="acDivider">|</span>');
+            }
+          }
+        });
+      }
+    });
 
   });
+
+  function suggestAutocompletion() {
+  }
+
 });
