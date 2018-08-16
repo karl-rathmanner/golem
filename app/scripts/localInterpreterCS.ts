294fc3ae8d8ed98d1dee1336409635c3edbad285
Karl Rathmanner <karl@rathmanner.org>
2018-08-16 17:43:46 +0200

    feat: add dom-manipulation context feature

--- app/scripts/localInterpreterCS.ts
+++ app/scripts/localInterpreterCS.ts
@@ -1,5 +1,6 @@
 import { unescape } from './schem/reader';
 import { Schem } from './schem/schem';
+import { SchemString, SchemNil } from './schem/types';
 
 export interface ContextArepResponse {
   result?: string;
@@ -7,25 +8,31 @@ export interface ContextArepResponse {
   contextId: number;
 }
 
-(function addInterpreter() {
-  const interpreter = new Schem();
-  window.golem.interpreter = interpreter;
-  window.golem.features.push('schem-interpreter');
+(async function addInterpreter() {
+  console.log('interpreter injected');
 
-  if (window.golem.injectedProcedures != null) {
-    window.golem.injectedProcedures.set('evalSchem', interpreter.evalSchem);
-    window.golem.injectedProcedures.set('arep', async (code: string, escape: boolean) => {
-      let resultOrError: ContextArepResponse = {
-        contextId: window.golem.contextId
-      };
+  // for *some* reason, the interpreter gets injected twice when preparing a context that explicitly requested the 'schem-interpreter' feature
+  // TODO: investigate
+  if (window.golem.interpreter == null) { 
+    window.golem.features.push('schem-interpreter');
+    const interpreter = new Schem();
+    window.golem.interpreter = interpreter;
 
-      await interpreter.arep(code).then(result => {
-        resultOrError.result = escape ? unescape(result) : result;
-      }).catch(reason => {
-        resultOrError.error = typeof reason === 'string' ? reason : JSON.stringify(reason);
+    if (window.golem.injectedProcedures != null) {
+      window.golem.injectedProcedures.set('evalSchem', interpreter.evalSchem);
+      window.golem.injectedProcedures.set('arep', async (code: string, escape: boolean) => {
+        let resultOrError: ContextArepResponse = {
+          contextId: window.golem.contextId
+        };
+
+        await interpreter!.arep(code).then(result => {
+          resultOrError.result = escape ? unescape(result) : result;
+        }).catch(reason => {
+          resultOrError.error = typeof reason === 'string' ? reason : JSON.stringify(reason);
+        });
+
+        return resultOrError;
       });
-
-      return resultOrError;
-    });
+    }
   }
 })();
