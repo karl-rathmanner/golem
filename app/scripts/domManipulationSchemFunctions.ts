a76413b1c4364c4fd0a9332f14b4c7501d461b81
Karl Rathmanner <karl@rathmanner.org>
2018-08-16 22:59:23 +0200

    feat(dom-manipulation): add "add-listener"

--- app/scripts/domManipulationSchemFunctions.ts
+++ app/scripts/domManipulationSchemFunctions.ts
@@ -1,4 +1,5 @@
-import { SchemNil, SchemString } from "./schem/types";
+import { SchemNil, SchemString, SchemType, SchemList } from './schem/types';
+import { pr_str } from './schem/printer';
 
 (function addSchemFunctions() {
   console.log(window.golem.interpreter!.replEnv);
@@ -10,6 +11,14 @@ import { SchemNil, SchemString } from "./schem/types";
         return SchemNil.instance;
       } else {
         throw new Error(`No object found with selector ${selector.valueOf()}`);
+      }
+    },
+    'add-listener': async (eventName: SchemString, selector: SchemString, ast: SchemType) => {
+      const elements = document.querySelectorAll<HTMLElement>(selector.valueOf());
+      console.log(ast);
+      // const code = await pr_str(ast);
+      for (let i = 0; i < elements.length; i++) {
+        elements.item(i).addEventListener(eventName.valueOf(), () => window.golem.interpreter!.evalSchem(ast));
       }
     }
   });
