38976a0a26c6921da887f10a1408168e4728f580
Karl Rathmanner <karl@rathmanner.org>
2018-10-17 12:55:28 +0200

    refactor: turn editor script into SchemEditor class

--- app/scripts/editor.ts
+++ app/scripts/editor.ts
@@ -1,5 +1,5 @@
 import * as monaco from 'monaco-editor';
-import { EventPageActionName, eventPageMessagingSchemFunctions } from './eventPageMessaging';
+import { eventPageMessagingSchemFunctions } from './eventPageMessaging';
 import { AddSchemSupportToEditor } from './monaco/schemLanguage';
 import { readStr, unescape as schemUnescape } from './schem/reader';
 import { filterRecursively, Schem } from './schem/schem';
@@ -9,92 +9,93 @@ import { VirtualFileSystem } from './virtualFilesystem';
 
 const example = require('!raw-loader!./schemScripts/example.schem');
 
-window.onload = () => {
+class SchemEditor {
+  public monacoEditor: monaco.editor.IStandaloneCodeEditor;
+  private evalZoneId: number;
+  private ast: AnySchemType;
+  
+  constructor () {
+    const interpreter = new Schem();
+    interpreter.replEnv.addMap(eventPageMessagingSchemFunctions);
+    interpreter.replEnv.addMap(this.editorManipulationSchemFunctions);
+    AddSchemSupportToEditor(interpreter);
+    
+    this.monacoEditor = monaco.editor.create(document.getElementById('monacoContainer')!, {
+      value: example,
+      language: 'schem',
+      theme: 'vs-dark'
+    });
+    
+    window.addEventListener('resize', this.updateEditorLayout );
+    this.updateEditorLayout();
+    this.monacoEditor.focus();
 
-  const messagingWithEventPage: {[symbolName in EventPageActionName]?: any} = eventPageMessagingSchemFunctions;
-  const editorManipulation = {
+    this.addFormEvaluationCommand(interpreter);
+  }
+
+  private editorManipulationSchemFunctions = {
     'editor-load-script': async (qualifiedFileName: SchemString) => {
       let candidate = await VirtualFileSystem.readObject(qualifiedFileName.valueOf());
       if (typeof candidate === 'string') {
-        editor.setValue(candidate);
+        this.monacoEditor.setValue(candidate);
         return SchemBoolean.true;
       } else {
         throw new Error(`Can only load strings into the editor.`);
       }
     },
     'editor-save-script': async (qualifiedFileName: SchemString) => {
-      const script = editor.getValue();
+      const script = this.monacoEditor.getValue();
       const result =  await VirtualFileSystem.createObject(qualifiedFileName.valueOf(), script, true)
-        .then(() => {
-          console.log('saved');
-          return new SchemString(`Successfully saved ${qualifiedFileName}.`);
-        }).catch(e => e);
+      .then(() => {
+        console.log('saved');
+        return new SchemString(`Successfully saved ${qualifiedFileName}.`);
+      }).catch(e => e);
       return result;
     }
   };
+  
+  private addFormEvaluationCommand(interpreter: Schem) {
+    this.monacoEditor.createContextKey('evalEnabled', true);
 
-  let interpreter = new Schem();
-  interpreter.replEnv.addMap(messagingWithEventPage);
-  interpreter.replEnv.addMap(editorManipulation);
-  let ast: AnySchemType;
-  AddSchemSupportToEditor(interpreter);
-
-  let editor = monaco.editor.create(document.getElementById('monacoContainer')!, {
-    value: example,
-    language: 'schem',
-    theme: 'vs-dark'
-  });
-
-  window.addEventListener('resize', () => { updateEditorLayout(editor); });
-  updateEditorLayout(editor);
-  editor.focus();
-
-  // remove evalViews and decorations when buffer changes
-  editor.getModel().onDidChangeContent((e) => {
-    evalDecoration = editor.deltaDecorations(evalDecoration, []);
-    editor.changeViewZones(function(changeAccessor) {
-      changeAccessor.removeZone(evalZoneId);
-    });
-  });
-
-  editor.createContextKey('evalEnabled', true);
-  let evalZoneId: number;
-  let evalDecoration = Array<string>();
-
-  let myKeyBinding = editor.addCommand(monaco.KeyMod.Alt | monaco.KeyCode.KEY_E, function() {
-
-    editor.changeViewZones(function(changeAccessor) {
-      let collectionRanges = getRangesOfSchemCollectionsAroundCursor();
-      let sourceOfInnermostCollection = editor.getModel().getValueInRange(collectionRanges[collectionRanges.length - 1]);
-      const viewZoneAfterLineNumber =  collectionRanges[1].endLineNumber; // collectionRanges[0] contains the '(do ...)' form, so we use the one a level below that
-
-      addEvaluationViewZone(viewZoneAfterLineNumber, '...evaluating...', 'evalWaitingForResultViewZone');
-
-      interpreter.arep(sourceOfInnermostCollection).then((result) => {
-        addEvaluationViewZone(viewZoneAfterLineNumber, schemUnescape(result), 'evalResultViewZone');
-      }).catch(error => {
-        console.error(error);
-        let errorMessage = (typeof error === 'string') ? error : error.error; // I still throw many plain strings as errors. This band-aid 'fixes' that.
-        if (typeof errorMessage !== 'string') {
-          errorMessage = JSON.stringify(errorMessage); // And this is a band-aid for the band-aid.
-        }
-        addEvaluationViewZone(viewZoneAfterLineNumber, errorMessage, 'evalErrorViewZone');
+    this.monacoEditor.addCommand(monaco.KeyMod.Alt | monaco.KeyCode.KEY_E, () => {
+      this.monacoEditor.changeViewZones((changeAccessor: any) => {
+        let collectionRanges = this.getRangesOfSchemCollectionsAroundCursor();
+        let sourceOfInnermostCollection = this.monacoEditor.getModel().getValueInRange(collectionRanges[collectionRanges.length - 1]);
+        const viewZoneAfterLineNumber = collectionRanges[1].endLineNumber; // collectionRanges[0] contains the '(do ...)' form, so we use the one a level below that
+        this.addEvaluationViewZone(viewZoneAfterLineNumber, '...evaluating...', 'evalWaitingForResultViewZone');
+        interpreter.arep(sourceOfInnermostCollection).then((result) => {
+          this.addEvaluationViewZone(viewZoneAfterLineNumber, schemUnescape(result), 'evalResultViewZone');
+        }).catch(error => {
+          console.error(error);
+          let errorMessage = (typeof error === 'string') ? error : error.error; // I still throw many plain strings as errors. This band-aid 'fixes' that.
+          if (typeof errorMessage !== 'string') {
+            errorMessage = JSON.stringify(errorMessage); // And this is a band-aid for the band-aid.
+          }
+          this.addEvaluationViewZone(viewZoneAfterLineNumber, errorMessage, 'evalErrorViewZone');
+        });
+        // handle decoration stuff
+        let evalDecoration = Array<string>();
+        // remove evalViews and decorations when buffer changes
+        this.monacoEditor.getModel().onDidChangeContent((e) => {
+          evalDecoration = this.monacoEditor.deltaDecorations(evalDecoration, []);
+          this.monacoEditor.changeViewZones((changeAccessor) => {
+            changeAccessor.removeZone(this.evalZoneId);
+          });
+        });
+        // highlight evaluated source range
+        evalDecoration = this.monacoEditor.deltaDecorations(evalDecoration, [
+          { range: collectionRanges[collectionRanges.length - 1], options: { inlineClassName: 'evaluatedSourceDecoration' } }
+        ]);
       });
+    }, 'evalEnabled');
+  }
 
-      // highlight evaluated source range
-      evalDecoration = editor.deltaDecorations(evalDecoration, [
-        { range: collectionRanges[collectionRanges.length - 1], options: { inlineClassName: 'evaluatedSourceDecoration' }}
-      ]);
-
-    });
-  }, 'evalEnabled');
-
-  function addEvaluationViewZone(afterLineNumber: number, content: string, className: string) {
+  private addEvaluationViewZone(afterLineNumber: number, content: string, className: string): void {
     let domNode = document.createElement('div');
     domNode.className = 'monaco-lines';
     domNode.classList.add('evalViewZone');
     domNode.classList.add(className);
-
+    
     // make sure content is always some kind of string
     if (typeof content !== 'string') {
       content = JSON.stringify(content);
@@ -102,19 +103,19 @@ window.onload = () => {
     // turn escaped newlines into actual newlines
     content = content.replace(/(\\r)?(\\n)/, '\n');
     const lines = content.split(/\n/g);
-
+    
     lines.forEach(line => {
       const lineNode = document.createElement('div');
       lineNode.textContent = line;
       domNode.appendChild(lineNode);
     });
-
-    let marginDomNode = createEvalMarginDomNode();
-
-    editor.changeViewZones(function(changeAccessor) {
-      changeAccessor.removeZone(evalZoneId);
-
-      evalZoneId = changeAccessor.addZone({
+    
+    let marginDomNode = this.createEvalMarginDomNode();
+    
+    this.monacoEditor.changeViewZones((changeAccessor) => {
+      changeAccessor.removeZone(this.evalZoneId);
+      
+      this.evalZoneId = changeAccessor.addZone({
         afterLineNumber: afterLineNumber,
         heightInLines: lines.length,
         domNode: domNode,
@@ -122,58 +123,59 @@ window.onload = () => {
       });
     });
   }
-
-  function createEvalMarginDomNode() {
+  
+  private createEvalMarginDomNode(): HTMLDivElement {
     let marginDomNode = document.createElement('div');
     marginDomNode.classList.add('line-numbers');
     marginDomNode.classList.add('evalMarginViewZone');
     marginDomNode.textContent = '=>';
     return marginDomNode;
   }
-
-  function getRangesOfSchemCollectionsAroundCursor(): monaco.Range[] {
-    const textModel = editor.getModel();
-    updateAst();
-    // offset cursorIndex by four to compensate for the 'do' form
-    const cursorIndex = textModel.getOffsetAt(editor.getPosition()) + 4;
-
-    // I'm sorry...
-    const collectionsAroundCursor = filterRecursively(ast, (element) => {
-      if (element != null && 'metadata' in element && element.metadata !== undefined &&
-          'sourceIndexStart' in element.metadata && typeof element.metadata.sourceIndexStart === 'number' &&
-          'sourceIndexEnd' in element.metadata && typeof element.metadata.sourceIndexEnd === 'number') {
-        return (element.metadata.sourceIndexStart < cursorIndex + 1 &&
-                element.metadata.sourceIndexEnd > cursorIndex - 1);
-      } else {
-        return false;
-      }
-    });
-
-    return collectionsAroundCursor.map(element => {
-      let metadata = (element as SchemList).metadata!;
-      // offset positions to compensate for the 'do' form (and make endPos include the last character)
-      let startPos = textModel.getPositionAt(metadata.sourceIndexStart! - 4);
-      let endPos = textModel.getPositionAt(metadata.sourceIndexEnd! - 3);
-      return new monaco.Range(
-        startPos.lineNumber, startPos.column,
-        endPos.lineNumber, endPos.column
-      );
-    });
-
-  }
-
-  function updateAst() {
+  
+  private updateAst() {
     try {
       // wrap source with a 'do' form so every list gets read
-      ast = readStr('(do ' + editor.getModel().getValue() + ')', true);
+      this.ast = readStr('(do ' + this.monacoEditor.getModel().getValue() + ')', true);
     } catch (e) {
       console.log(e);
     }
   }
-};
-
-function updateEditorLayout(editor: monaco.editor.IStandaloneCodeEditor) {
-  getHTMLElementById('monacoContainer').style.height = `${window.innerHeight}px`;
-  getHTMLElementById('monacoContainer').style.width = `${window.innerWidth}px`;
-  editor.layout();
+  
+  private getRangesOfSchemCollectionsAroundCursor(): monaco.Range[] {
+    const textModel = this.monacoEditor.getModel();
+    this.updateAst();
+    // offset cursorIndex by four to compensate for the 'do' form
+    const cursorIndex = textModel.getOffsetAt(this.monacoEditor.getPosition()) + 4;
+    
+    // I'm sorry...
+    const collectionsAroundCursor = filterRecursively(this.ast, (element) => {
+      if (element != null && 'metadata' in element && element.metadata !== undefined &&
+      'sourceIndexStart' in element.metadata && typeof element.metadata.sourceIndexStart === 'number' &&
+      'sourceIndexEnd' in element.metadata && typeof element.metadata.sourceIndexEnd === 'number') {
+        return (element.metadata.sourceIndexStart < cursorIndex + 1 && element.metadata.sourceIndexEnd > cursorIndex - 1);
+      } else {
+        return false;
+      }
+    });
+    
+    return collectionsAroundCursor.map(element => {
+      let metadata = (element as SchemList).metadata!;
+      
+      // offset positions to compensate for the 'do' form (and make endPos include the last character)
+      let startPos = textModel.getPositionAt(metadata.sourceIndexStart! - 4);
+      let endPos = textModel.getPositionAt(metadata.sourceIndexEnd! - 3);
+      
+      return new monaco.Range(startPos.lineNumber, startPos.column, endPos.lineNumber, endPos.column);
+    });
+  }
+  
+  private updateEditorLayout() {
+    getHTMLElementById('monacoContainer').style.height = `${window.innerHeight}px`;
+    getHTMLElementById('monacoContainer').style.width = `${window.innerWidth}px`;
+    this.monacoEditor.layout();
+  }
 }
+
+window.onload = () => {
+  new SchemEditor();
+};
