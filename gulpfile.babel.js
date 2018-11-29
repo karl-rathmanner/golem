ba8c4a0c16344fd32528af8ffd8b4f5cafed674a
Karl Rathmanner <karl@rathmanner.org>
2018-11-29 12:55:55 +0100

    fix(gulp): move pack definition to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,23 +11,13 @@ import { scripts } from './tasks/scripts';
 import { styles } from './tasks/styles';
 import { changelog } from './tasks/changelog';
 
+// import gulp from 'gulp'
+import { colors, log } from 'gulp-util'
+import zip from 'gulp-zip'
+import packageDetails from './package.json'
+import args from './tasks/lib/args'
 
-let Server = require('karma').Server
-
-export const test = (done) => {
-  new Server({
-    configFile: require('path').resolve('karma.conf.js')
-  }, (err) => {
-    if (err === 0) {
-      done();
-    } else {
-      done(new gutil.PluginError('karma', {
-        message: `Karma Tests failed: ${err}`
-      }));
-    }
-  }).start();
-};
-
+/// building
 export const build = gulp.series(
   clean, 
   gulp.parallel(
@@ -45,3 +35,47 @@ export const build = gulp.series(
 );
 
 export default build;
+
+/// testing
+
+let Server = require('karma').Server
+
+export const test = (done) => {
+  new Server({
+    configFile: require('path').resolve('karma.conf.js')
+  }, (err) => {
+    if (err === 0) {
+      done();
+    } else {
+      done(new gutil.PluginError('karma', {
+        message: `Karma Tests failed: ${err}`
+      }));
+    }
+  }).start();
+};
+
+/// packing
+
+function getPackFileType () {
+  switch (args.vendor) {
+    case 'firefox':
+      return '.xpi'
+    default:
+      return '.zip'
+  }
+}
+
+export const pack = gulp.series(build, () => {
+  const name = packageDetails.name;
+  const version = packageDetails.version;
+  const filetype = getPackFileType();
+  const filename = `${name}-${version}-${args.vendor}${filetype}`;
+  return gulp.src(`dist/${args.vendor}/**/*`)
+    .pipe(zip(filename))
+    .pipe(gulp.dest('./packages'))
+    .on('end', () => {
+      let distStyled = colors.magenta(`dist/${args.vendor}`)
+      let filenameStyled = colors.magenta(`./packages/${filename}`)
+      log(`Packed ${distStyled} to ${filenameStyled}`)
+    });
+});
