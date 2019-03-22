4c507f1119727526e88f7c50cc57461da1140c3d
Karl Rathmanner <karl@rathmanner.org>
2019-03-22 18:28:10 +0100

    feat: add links and context information to popup

--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -1,3 +1,18 @@
-// Enable chromereload by uncommenting this line:
-// import 'chromereload/devonly'
-// Not doing anything yet.
+import 'chromereload/devonly'
+import { browser } from 'webextension-polyfill-ts';
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const infoElement = document.getElementById('info');
+  if (infoElement != null) {
+    const bgp = await browser.runtime.getBackgroundPage();
+    const state = bgp.golem.priviledgedContext!.globalState;
+    const currentWindow = await browser.windows.getCurrent();
+    const activeTabsInWindow = await browser.tabs.query({active: true, windowId: currentWindow.id});
+    const localContextIds = state.contextManager.getMatchingContextIds({tabId: activeTabsInWindow[0].id, windowId: currentWindow.id})
+    console.log(bgp, currentWindow, activeTabsInWindow, localContextIds);
+
+    infoElement.innerHTML = localContextIds.length > 0 ? 
+        'Active contexts in this tab:<br/>' + localContextIds.join(', '):
+        'No active contexts in this tab.';
+  }});
